0fa977928f72e9e0d7f9a352ec208de5c3d77549
pradnya <pradnyahegde@gmail.com>
2020-01-13 14:00:46 -0500

    replaced message string with this.state.message

--- clicky-game/src/components/FriendCard/index.js
+++ clicky-game/src/components/FriendCard/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./style.css";
 
-function FriendCard(props) {
-  return (
-    <div className="card">
+const FriendCard = props=> (
+  
+    <div className="card col-md-3 bg-secondary" onClick={()=>props.clickCount(props.id)}>
       <div className="img-container">
         <img alt={props.name} src={props.image} />
       </div>
     </div>
-  );
-}
+  
+)
 
 export default FriendCard;
