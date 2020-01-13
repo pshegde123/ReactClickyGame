bcb9d3007b7ee64650127b4a0daa874adee6cc36
pradnya <pradnyahegde@gmail.com>
2020-01-13 14:03:11 -0500

    removed class='title'

--- clicky-game/src/components/Navbar/index.js
+++ clicky-game/src/components/Navbar/index.js
@@ -3,17 +3,17 @@ import "./style.css";
 
 function Navbar(props) {
   return (
-    <div className="title">
+    
         <nav className="navbar">
             <ul>
                 <li>
                     <a className="title" href="/">Clicky Game</a>
                 </li>
-                <li>Click an image to begin!</li>
-                <li>Score: {props.score} | TopScore: {props.topscore}</li>
+                <li>{props.message}</li>
+                <li>Score: {props.score} | TopScore: {props.topScore}</li>
             </ul>
         </nav>
-    </div>);
+    );
 }
 
 export default Navbar;
